88a50fbebc5298d545fd7e2fdf4a875c5f6c0ac4
isaacans <isaacans@gmail.com>
2024-05-20 17:13:12 +1200

    Add limit to single number user input and change how input string is updated
    Input limited to avoid calculator screen overflow. Moved updating of user input string to a separate function to simplify code and improve code readability

--- code.js
+++ code.js
@@ -1,59 +1,55 @@
 let isPositive = true;
-let calcOutputString = "";
+let calcInputString = "";
 const calcScreen = document.getElementById('calculator_screen');
 const calcBorder = document.getElementById('calculator_border');
 const buttons = document.querySelectorAll('button');
 const button_1 = document.getElementById('one')
 
 calcBorder.addEventListener("click", (clickEvent) => {
-    console.log(clickEvent.target.id);
     processButton(clickEvent.target.id);
     updateScreen();
 });
 
-console.log(button_1);
-
 function processButton(id) {
     switch(id) {
         case 'one':
-            calcOutputString += 1;
+            updateInputString(1);
             break;
         case 'two':
-            calcOutputString += 2;
+            updateInputString(2);
             break;
         case 'three':
-            calcOutputString += 3;
+            updateInputString(3);
             break;
         case 'four':
-            calcOutputString += 4;
+            updateInputString(4);
             break;        
         case 'five':
-            calcOutputString += 5;
+            updateInputString(5);
             break;    
         case 'six':
-            calcOutputString += 6;
+            updateInputString(6);
             break;
         case 'seven':
-            calcOutputString += 7;
+            updateInputString(7);
             break;    
         case 'eight':
-            calcOutputString += 8;
+            updateInputString(8);
             break;    
         case 'nine':
-            calcOutputString += 9;
+            updateInputString(9);
             break;    
         case 'zero':
-            calcOutputString += 0;
+            updateInputString(0);
             break;  
         case 'AC':
-            calcOutputString = '';
-            isPositive = true;
+            updateInputString('AC')
             break;        
         case 'flip_positivity':
             flipPositivity();
             break;    
         case 'backspace':
-            calcOutputString = calcOutputString.slice(0, -1);
+            updateInputString('backspace');
             break;    
         case 'divide':
             console.log(id);
@@ -76,18 +72,27 @@ function processButton(id) {
     };
 };
 
-function updateScreen(input) {
-    if (calcOutputString.length <= 15) {
-        calcScreen.textContent = calcOutputString;
+function updateScreen() {
+        calcScreen.textContent = calcInputString;
+};
+
+function updateInputString(input) {
+    if (input === 'backspace'){
+        calcInputString = calcInputString.slice(0, -1);
+    } else if (input === 'AC'){
+        calcInputString = '';
+        isPositive = true; // To reset variable to default positive.
+    } else if (calcInputString.length <= 15) {
+        calcInputString += input;
     };
 };
 
 function flipPositivity() {
     if (isPositive === true) {
-        calcOutputString = '-' + calcOutputString;
+        calcInputString = '-' + calcInputString;
         isPositive = false;
     } else if (isPositive === false) {
-        calcOutputString = calcOutputString.replace('-', '');
+        calcInputString = calcInputString.replace('-', '');
         isPositive = true;
     };
 };
